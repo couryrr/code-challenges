d2f472ef8a6d42ab874f463e41d7ccf5359a25df
Coury Richards <2702992+couryrr@users.noreply.github>
2024-12-05 19:59:52 -0500

    updated: 1 gold star for day 4 :(
    Signed-off-by: Coury Richards <2702992+couryrr@users.noreply.github>

--- advent-of-code/2024/day_4.go
+++ advent-of-code/2024/day_4.go
@@ -1,52 +1,117 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"strings"
 )
+
+var input [][]byte
+var word []byte
 
 func main() {
 	url := "https://adventofcode.com/2024/day/4/input"
 	file_name := "day_four.txt"
-	input := strings.Split(string(GetDataForDay(url, file_name)), "\n")
+	input = bytes.Split(GetDataForDay(url, file_name), []byte("\n"))
 
-	word := []byte("XMAS")
-	count := 0	
+	word = []byte("XMAS")
+	fmt.Println(search_one())
+	
+	//not correct yet...
+	word = []byte("MAS")
+	fmt.Println(search_two())
+}
 
-	for r := range input {
-		for c := range input[r] {
-			if dfs(input, word, r, c, 0){
+func search_one() int{
+	count := 0
+	for r := range len(input) {
+		for c := range len(input[r]) {
+			if dfs(r, c, 0, 1, 0){
+				count++
+			}
+
+			if dfs(r, c, 0, -1, 0){
+				count++
+			}
+
+			if dfs(r, c, -1, 0, 0){
+				count++
+			}
+			
+			if dfs(r, c, 1, 0, 0){
+				count++
+			}
+
+			if dfs(r, c, 1, -1, 0) {
+				count++
+			}
+
+			if dfs(r, c, 1, 1, 0) {
+				count++
+			}
+			
+			if dfs(r, c, -1, -1, 0) {
+				count++
+			}
+
+			if dfs(r, c, -1, 1, 0) {
 				count++
 			}
 		}
 	}
-	fmt.Println(count)
+	return count
 }
 
-func dfs(input []string, word []byte, row, col, word_idx int) bool {
+func search_two() int {
+	count := 0
+	for r := range len(input) {
+		for c := range len(input[r]) {
+			if input[r][c] == 'A' {
+				if r - 1 < 0 || r + 1 >= len(input) { 
+					break
+				}
+
+				if c - 1 < 0 || c + 1 >= len(input[r+1]) {
+					break	
+				}
+				
+				c1 := input[r-1][c-1]
+				c2 := input[r+1][c+1]
+				idk1 := (c1 == 'M' || c1 == 'S') && (c1 != c2)
+
+				c3 := input[r-1][c+1]
+				c4 := input[r+1][c-1]
+				idk2 := (c3 == 'M' || c3 == 'S') && (c3 != c4)
+
+				if idk1 && idk2 {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
+func dfs(row, col, dr, dc, word_idx int) bool {
 	if word_idx == len(word) {
 		return true
 	}
 	
-	row_overflow := row < 0 || row >= len(input)
-	col_overflow := col < 0 || col >= len(input[row])
-
-	if row_overflow || col_overflow {
-		return false
+	if row < 0 || row >= len(input) { 
+		return false 
 	}
 
-	letters := []byte(input[row])
-	letter := letters[col]
-	if letter != word[word_idx] {
+	if col < 0 || col >= len(input[row]) {
+		return false 
+	}
+
+	if input[row][col]!= word[word_idx] {
 		return false	
 	}
-	
-	letters[col] = byte('%')
-	if dfs(input, word, row, col - 1, word_idx + 1) || // why can't this find overlaps?
-		dfs(input, word, row, col + 1, word_idx + 1) {
+
+	if dfs(row + dr, col + dc, dr, dc, word_idx + 1) { 
 		return true
 	}
-	letters[col] = letter
 
 	return false
 }
+
