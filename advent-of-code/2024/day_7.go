2b088b43e3b8e408a9b7ae4e6e567417dbb45bd0
Coury Richards <2702992+couryrr@users.noreply.github>
2024-12-07 21:46:16 -0500

    added: day 7 part one
    Signed-off-by: Coury Richards <2702992+couryrr@users.noreply.github>

---
+++ advent-of-code/2024/day_7.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+var values []int
+func main() {
+	url := "https://adventofcode.com/2024/day/7/input"
+	file_name := "day_seven.txt"
+
+	input := GetDataForDay(url, file_name)
+	
+	lines := bytes.Split(input, []byte("\n"))
+	total := 0	
+	for i := range len(lines)-2 {
+		parts := bytes.Split(lines[i], []byte(": "))
+		goal, _ := strconv.Atoi(string(parts[0]))
+		data := bytes.Split(parts[1], []byte(" "))
+		for ii := range len(data) {
+ 			v, _ := strconv.Atoi(string(data[ii]))
+			values = append(values, v)
+		}
+
+		if eval(values[0], goal, 1){
+			total += goal
+		}
+		values = nil
+	}
+	fmt.Println(total)
+}
+
+
+func eval(total, goal, pos int) bool {
+	if total == goal && pos == len(values) {
+		return true
+	}
+	
+	if pos + 1 > len(values)  {
+		return false
+	}
+
+	if eval(total + values[pos], goal, pos+1) {
+		return true
+	}
+
+	if eval(total * values[pos], goal, pos+1) {
+		return true
+	}
+
+	return false
+
+}
