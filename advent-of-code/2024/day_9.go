4253b909726fdae19fe148a52ac30b17795c2707
Coury Richards <2702992+couryrr@users.noreply.github>
2024-12-11 21:57:32 -0500

    updated: part 2 is not clicking
    Signed-off-by: Coury Richards <2702992+couryrr@users.noreply.github>

--- advent-of-code/2024/day_9.go
+++ advent-of-code/2024/day_9.go
@@ -12,25 +12,39 @@ func main() {
 	file_name := "day_nine.txt"
 
 	input := bytes.TrimSpace(GetDataForDay(url, file_name))
-	expanded := make([]int, 0, 10)
-	file_id := 0
-	isFree := false
-	for i, v := range input {
-		vv, _ := strconv.Atoi(string(v))
-		isFree = (i+1)%2 == 0
-		for ii := 0; ii < vv; ii++ {
-			if isFree {
-				expanded = append(expanded, -1)
-			} else {
-				expanded = append(expanded, file_id)
-			}
-		}
-		if !isFree {
-			file_id++
-		}
-		isFree = false
-	}
 	
+	expanded, freeSapces := expand(input)
+//	s1 := make([]int, len(expanded))   
+//	copy(s1, expanded)
+//	solution_d9_1(s1)
+
+	end := len(expanded) - 1
+	fmt.Printf("%v\n", freeSapces)
+	for k, v := range freeSapces {
+		fmt.Println(k, v)
+	}
+
+	for end >= 0 {
+		if expanded[end] != -1 {
+			curr_value := expanded[end]
+			curr_end := end
+			curr_len := 0
+
+			for end >= 0 && expanded[end] == curr_value {
+				curr_len++
+				end--
+			}
+			fmt.Printf("for %d the len is %d and end is %d\n",  curr_value, curr_len, curr_end)
+
+				} else {
+			end--
+		}
+	}
+
+	fmt.Printf("%v\n", expanded)
+}
+
+func solution_d9_1(expanded []int){
 	free := slices.Index(expanded, -1)
 	end := len(expanded) - 1
 
@@ -43,12 +57,44 @@ func main() {
 		end--
 	}
 
-	s1 := 0
+	s1 := checksum(expanded)
+
+	fmt.Println(s1)
+}
+
+func expand(input []byte) ([]int, map[int]int){
+	freeSapces := make(map[int]int, 1024)
+	expanded := make([]int, 0, 1024)
+	file_id := 0
+	isFree := false
+	for i, v := range input {
+		vv, _ := strconv.Atoi(string(v))
+		isFree = (i+1)%2 == 0
+		if isFree {
+			freeSapces[len(expanded)] = vv
+		}
+		for ii := 0; ii < vv; ii++ {
+			if isFree {
+				expanded = append(expanded, -1)
+			} else {
+				expanded = append(expanded, file_id)
+			}
+		}
+		if !isFree {
+			file_id++
+		}
+		isFree = false
+	}
+	return expanded, freeSapces
+}
+
+func checksum(expanded []int) int {
+	checksum := 0
 	for i, v := range expanded {
 		if v == -1 {
 			break
 		}
-		s1 += i*v
+		checksum += i*v
 	}
-	fmt.Println(s1)
+	return checksum
 }
